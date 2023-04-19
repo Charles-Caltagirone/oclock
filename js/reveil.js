f1c9d4e21074b90b44fa80b4e83b08bd367f49dd
Charles-Caltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-04-19 12:25:05 +0200

    test pour le réveil mais toujours pas d'actualisation

--- js/reveil.js
+++ js/reveil.js
@@ -1,7 +1,8 @@
 let timeDiv = document.getElementById("timeDiv");
 let warningDiv = document.getElementById("warning");
 let confirmBtn = document.getElementById("confirm");
-// let interval;
+let interval;
+// let alarmRegisted;
 
 function Time() {
   let currentClock = new Date();
@@ -23,65 +24,78 @@ function checkTime(nombre) {
   return nombre;
 }
 
-function affichage() {
-  // confirmBtn.addEventListener("click", () => {
-    let currentTime = Time();
-    console.log("okkk");
-    const newP = document.createElement("p");
+confirmBtn.addEventListener("click", () => {
+  affichage();
+});
 
-    // confirm.onclick = function () {
-    let alarmList = document.getElementById("alarmList");
-    let heure = document.getElementById("heure");
-    let alarmText = document.getElementById("alarmText");
-    // alarmList.innerHTML = "";
+function timeRemaining() {
+  let currentTime = Time();
+  let newP = document.createElement("p");
 
-    let confirmAlarm = heure.value;
-    let confirmText = alarmText.value;
-    let calculCurrentDate = new Date(`2000-01-01T${currentTime}`);
-    let calculAlarmDate = new Date(`2000-01-01T${heure.value}`);
-    let timeLeft = Math.abs((calculAlarmDate - calculCurrentDate) / 1000 / 60);
-    let tempsRestant = Math.ceil(timeLeft);
+  // confirm.onclick = function () {
+  console.log("okkk");
+  let alarmList = document.getElementById("alarmList");
+  let heure = document.getElementById("heure");
+  let alarmText = document.getElementById("alarmText");
+  // alarmList.innerHTML = "";
 
-    if (confirmAlarm == "") {
-      warningDiv.innerText = "Choisir une heure !";
-    } else if (confirmText == "") {
-      warningDiv.innerText = "Remplir une description !";
-    } else {
-      if (confirmAlarm > currentTime) {
-        newP.innerHTML =
-          confirmAlarm +
-          " : " +
-          confirmText +
-          " : A venir. Temps restant : " +
-          timeLeft +
-          " min";
-        alarmList.appendChild(newP);
-
-        warningDiv.innerText = "";
-        // clearInterval(interval);
-        // interval = setInterval(timeLeft, 1000);
-      } else if (confirmAlarm < currentTime) {
-        warning.innerText = "Choisir une heure postérieure !";
-      } else {
-        newP.innerHTML = "Heure passée";
-        alarmList.appendChild(newP);
-        warningDiv.innerText = "";
-      }
-    }
-  // });
+  let confirmAlarm = heure.value;
+  let confirmText = alarmText.value;
+  let calculCurrentDate = new Date(`2000-01-01T${currentTime}`);
+  let calculAlarmDate = new Date(`2000-01-01T${heure.value}`);
+  let timeLeft = Math.abs((calculAlarmDate - calculCurrentDate) / 1000 / 60);
+  let tempsRestant = Math.ceil(timeLeft);
+  // return timeLeft;
 }
-  // };
-  // clearInterval(interval);
-  // confirmAlarm = "";
-  // confirmText = "";
+
+function affichage(confirmAlarm, confirmText, currentTime, newP) {
+  timeRemaining();
+  // let interval = setInterval(() => {
+
+  if (confirmAlarm == "") {
+    warningDiv.innerText = "Choisir une heure !";
+  } else if (confirmText == "") {
+    warningDiv.innerText = "Remplir une description !";
+  } else {
+    if (confirmAlarm > currentTime) {
+      alarmRegisted =
+        confirmAlarm +
+        " : " +
+        confirmText +
+        " : A venir. Temps restant : " +
+        timeLeft +
+        " min";
+      newP.innerHTML = alarmRegisted;
+      alarmList.append(newP);
+      // setInterval(affichage);
+
+      // clearInterval(interval);
+      // warningDiv.innerText = "";
+      // interval = setInterval(timeLeft, 1000);
+    } else if (confirmAlarm < currentTime) {
+      warning.innerText = "Choisir une heure postérieure !";
+    } else {
+      newP.innerHTML = "Heure passée";
+      alarmList.append(newP);
+      // warningDiv.innerText = "";
+    }
+  }
+  // }, 1000);
+  // setInterval(affichage, 1000);
+  // return alarmRegisted;
+}
+// affichage();
+// };
+// clearInterval(interval);
+// confirmAlarm = "";
+// confirmText = "";
 // function afficher() {
 //   confirmBtn.addEventListener("click", affichage());
 
 // }
 
-let interval = setInterval(() => {
-  Time();
-  affichage();
-  // clearInterval(interval);
-}, 1000);
-
+// setInterval(() => {
+//   Time();
+// affichage();
+//   // clearInterval(interval);
+// }, 1000);
